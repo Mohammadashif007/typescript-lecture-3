0c3a375615afe0a21f70bd79c6f8e464d416710f
Mohammad Ashif <Mohammad Ashif>
2024-04-23 14:05:50 +0600

    typescript types

---
+++ index.js
@@ -0,0 +1,9 @@
+var char = "john";
+// char = 33;
+var isNight = false;
+isNight = true;
+var num = 44;
+var multiply = function (num) {
+    return num * Math.PI;
+};
+console.log(multiply(100));
---
+++ index.ts
@@ -0,0 +1,15 @@
+let char = "john";
+
+// char = 33;
+
+let isNight = false;
+
+isNight = true;
+
+let num = 44;
+
+const multiply = (num: number) => {
+    return num * Math.PI;
+};
+
+console.log(multiply(100));
